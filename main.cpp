84cfd6d0d6368616953ea7903daf9870893214e4
Andrii Heonia <andrii.heonia@here.com>
2016-03-07 14:47:14 +0100

    EM_ASM version added

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 using namespace emscripten;
 
 // Callback class (instance of ths class should be called from js)
-class Callback {
+class Callback
+{
   public:
-    ~Callback () {
+    ~Callback()
+    {
       printf("Callback dies! \n");
     };
-    void sayHi () {
+    void sayHi()
+    {
       printf("Hello, I'm callback! \n");
     };
 };
@@ -19,14 +22,32 @@ EMSCRIPTEN_BINDINGS(Callback)
       .function("sayHi", &Callback::sayHi);
 }
 
-//Callback *cb = new Callback(); // global raw pointer to the callback
-Callback cb = Callback(); // global callback
-
 // App class (entry point)
-class App {
+class App
+{
   public:
-    void run() {
-      val::global("window").call<void>("sendRequest", cb);      
+    App()
+    {
+      m_cb = new Callback();
+    }
+    ~App()
+    {
+      printf("App dies! \n");
+      delete m_cb;
+    }
+    void run(val requestManager)
+    {
+      EM_ASM_ARGS({
+        // TODO: how to not use global sendRequest? I'd like to do something like this:
+        // requestManager.sendRequest($0, $1);
+        sendRequest($0, $1);
+      }, &App::callbackWrapFunc, m_cb);
+    }
+  private:
+    Callback *m_cb;
+    static void callbackWrapFunc(Callback *cb)
+    {
+      cb->sayHi();
     }
 };
 EMSCRIPTEN_BINDINGS(App)

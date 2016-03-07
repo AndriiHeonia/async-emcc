7207aebdcc65b5d183f1a26516ccdd77eb06ce8e
Andrii Heonia <andrii.heonia@here.com>
2016-03-07 12:39:06 +0100

    fix demo

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ using namespace emscripten;
 // Callback class (instance of ths class should be called from js)
 class Callback {
   public:
+    ~Callback () {
+      printf("Callback dies! \n");
+    };
     void sayHi () {
       printf("Hello, I'm callback! \n");
     };
@@ -16,14 +19,13 @@ EMSCRIPTEN_BINDINGS(Callback)
       .function("sayHi", &Callback::sayHi);
 }
 
-// global raw pointer to the callback instance
-Callback *cb;
+//Callback *cb = new Callback(); // global raw pointer to the callback
+Callback cb = Callback(); // global callback
 
 // App class (entry point)
 class App {
   public:
     void run() {
-      cb = new Callback();
       val::global("window").call<void>("sendRequest", cb);      
     }
 };
